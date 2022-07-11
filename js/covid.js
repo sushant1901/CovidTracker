b6d01cb7e0e0cbe5999de3a0d9917755953def68
sushant1901 <mishrasushant1901@gmail.com>
2022-07-11 20:31:32 +0530

    Covid Tracker

---
+++ js/covid.js
@@ -0,0 +1,21 @@
+const api_url="https://api.covid19api.com/summary";
+let getAPI=(URL) =>{
+    fetch(URL)
+    .then((res) => {return res.json()})
+    .then((data) =>{console.log(data);
+        let country=data.Countries[77].Country;
+        let totalCases=data.Countries[77].TotalConfirmed;
+        let totalDeaths=data.Countries[77].TotalDeaths;
+        let newCase=data.Countries[77].NewConfirmed;
+        let newDeaths=data.Countries[77].NewDeaths;
+        let date=data.Countries[77].NewDeaths;
+        let today=new Date().toDateString();
+        displayData(country,totalCases,totalDeaths,newCase,newDeaths,today);})
+    .catch((error) => {console.log("error message");});
+};
+getAPI(api_url);
+function displayData(c,tc,td,nc,nd,t){
+let pre=document.createElement("pre");
+pre.innerHTML = `<span>Country Name: </span> ${c}\n<span>Date: </span> ${t} \n<span>Total Cases: </span> ${tc} \n<span>Total Deaths: </span>${td} \n<span>New Cases: </span>${nc} \n<span>New Deaths: </span>${nd}`;
+document.getElementById("box").appendChild(pre);
+}
